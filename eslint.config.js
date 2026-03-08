0078e0754ddb6784df4d63db89da5d3778865840
vladaworkflow-ops <vlada.work.flow@gmail.com>
2026-03-08 16:26:34 +0100

    chore: add ts integration, install vite, linter and eslint

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import parser from "@typescript-eslint/parser";
+
+export default [
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        project: "./tsconfig.json"
+      }
+    },
+    plugins: {
+      "@typescript-eslint": tseslint
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+
+      "@typescript-eslint/no-explicit-any": "error"
+    }
+  }
+];
