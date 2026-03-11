b26695e0fb20eae56c38929e99d44b8d44d288e2
vladaworkflow-ops <vlada.work.flow@gmail.com>
2026-03-11 23:45:11 +0100

    feat: implement user-menu filling and toggling logic functions

---
+++ src/scripts/utils/user-menu.ts
@@ -0,0 +1,55 @@
+import { authState } from '../api/auth';
+import {openPopup, closeModal} from '../popup/popup-user';
+import { UserAuth } from '../../types/user';
+
+const userMenu = document.querySelector('.user-menu') as HTMLDivElement;
+const userNameEl = userMenu?.querySelector('.user-menu-name') as HTMLSpanElement;
+const userEmailEl = userMenu?.querySelector('.user-menu-email') as HTMLSpanElement;
+const userIcon = document.querySelector('.user-icon') as HTMLElement | null;
+const overlay = document.querySelector('.overlay') as HTMLElement | null;
+const btnSignIn  = document.querySelector('.sign_in-btn') as HTMLElement | null;
+const btnSignOut = document.querySelector('.sign_out-btn') as HTMLElement | null;
+const loginPopup = document.querySelector('.login-popup') as HTMLElement | null;
+const popupUser = document.querySelector('.popup-user') as HTMLElement | null;
+const closeUserMenu = document.querySelector('.close-popup-user-menu') as HTMLElement | null;
+
+closeUserMenu?.addEventListener('click', () => {
+  if (!overlay || !userMenu || !userIcon) return;
+  closeModal(overlay, userMenu);
+})
+
+btnSignIn?.addEventListener('click', () => {
+  if (!overlay || !loginPopup) return;
+  openPopup(overlay, loginPopup);
+  popupUser?.classList.remove('active-modal');
+})
+
+btnSignOut?.addEventListener('click', () => {
+  authState.isLogged = false;
+  if (!overlay || !userMenu || !userIcon) return;
+  closeModal(overlay, userMenu);
+  updateUserMenu(authState);
+  updateUserIcon(authState);
+})
+
+
+export function updateUserMenu(auth: UserAuth | null) {
+  if (!userNameEl || !userEmailEl) return;
+
+  if (auth?.isLogged && auth.user) {
+    userNameEl.textContent = auth.user.name;
+    userEmailEl.textContent = auth.user.email;
+  } else {
+    userNameEl.textContent = '';
+    userEmailEl.textContent = '';
+  }
+}
+
+export function updateUserIcon(auth: UserAuth | null) {
+  const userIcon = document.querySelector('.user-icon') as HTMLElement | null;
+  if (!userIcon) return;
+
+  userIcon.style.backgroundImage = auth?.isLogged
+    ? 'url("../../assets/icons/user-logged.png")'
+    : 'url("../../assets/icons/user-in.png")';
+}
