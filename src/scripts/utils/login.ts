88ca0b3e470a2f1cd7155048f160ae52996036ca
vladaworkflow-ops <vlada.work.flow@gmail.com>
2026-03-11 23:43:51 +0100

    feat: implement login logic functions

---
+++ src/scripts/utils/login.ts
@@ -0,0 +1,78 @@
+import { loginUser } from "../api/auth";
+import { LoginUser } from "../../types/user";
+import { setupValidation } from "./registration";
+import { authState }from '../api/auth';
+import { validateLogin, validatePassword } from "../utils/validation";
+import { BackendMessages } from "../utils/backendMessages";
+import { updateUserMenu, updateUserIcon } from "./user-menu";
+
+const loginContainer = document.querySelector('.login-popup') as HTMLDivElement;
+const form = document.getElementById('form-login') as HTMLFormElement;
+const loginInput = document.getElementById('log-in') as HTMLInputElement;
+const passwordInput = document.getElementById('password-login') as HTMLInputElement;
+const loginButton = document.querySelector('.submit-login-form') as HTMLButtonElement;
+loginButton.disabled = true;
+
+const loginError = document.getElementById('log-in-error') as HTMLDivElement;
+const passwordError = document.getElementById('password-error-login') as HTMLDivElement;
+const formErrorGlobal = document.querySelector('.form-login-error-global') as HTMLDivElement;
+
+function checkFormLoginValidity() {
+
+  const loginValidation = validateLogin(loginInput.value);
+  const passwordValidation = validatePassword(passwordInput.value);
+
+  loginButton.disabled = !!(
+    loginValidation ||
+    passwordValidation
+  );
+}
+
+setupValidation(loginInput, loginError, validateLogin, checkFormLoginValidity);
+setupValidation(passwordInput, passwordError, validatePassword, checkFormLoginValidity);
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  if (loginButton.disabled) return;
+
+  try{
+    const response = await loginUser({
+    login: loginInput.value,
+    password: passwordInput.value,
+    } as LoginUser )
+
+    const responseData = await response.json();
+
+    const message =
+      BackendMessages.login[
+        response.status as keyof typeof BackendMessages.login
+      ] || BackendMessages.login.default;
+
+     if (response.status === 200) {
+
+      const { user } = responseData.data;
+
+      authState.isLogged = true;
+      authState.user = {
+        name: user.name,
+        email: user.email
+      };
+
+      formErrorGlobal.textContent = message;
+
+      updateUserMenu(authState);
+      updateUserIcon(authState);
+
+      form.reset();
+      loginContainer.classList.remove('active-modal');
+
+    } else {
+      formErrorGlobal.textContent = message;
+    }
+
+  } catch (error) {
+    formErrorGlobal.textContent = BackendMessages.registration.default;
+  }
+})
+
