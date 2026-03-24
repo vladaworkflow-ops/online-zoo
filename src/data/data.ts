ec6559ad9226087524f7016a43e556c04d15c78a
vladaworkflow-ops <vlada.work.flow@gmail.com>
2026-03-24 17:26:02 +0100

    feat: create data object for cards

---
+++ src/data/data.ts
@@ -0,0 +1,150 @@
+const BASE = import.meta.env.BASE_URL;
+
+interface cardData {
+  "id": number
+  "petImg": string
+  "petIcon": string
+}
+
+export const cardData: cardData[] = [
+  {
+    id: 1,
+    petImg: `${BASE}assets/images/cards/panda-card.png`,
+    petIcon: ''
+  },
+  {
+    id: 2,
+    petImg: `${BASE}assets/images/cards/lemur-card.png`,
+    petIcon: ''
+  },
+  {
+    id: 3,
+    petImg: `${BASE}assets/images/cards/gorilla-card.png`,
+    petIcon: ''
+  },
+  {
+    id: 4,
+    petImg: `${BASE}assets/images/cards/crocodale-card.png`,
+    petIcon: ''
+  },
+  {
+    id: 5,
+    petImg: `${BASE}assets/images/cards/eagles-card.png`,
+    petIcon: ''
+  },
+  {
+    id: 6,
+    petImg: `${BASE}assets/images/cards/coala-card.png`,
+    petIcon: ''
+  },
+  {
+    id: 7,
+    petImg: `${BASE}assets/images/cards/lion-card.png`,
+    petIcon: ''
+  },
+  {
+    id: 8,
+    petImg: `${BASE}assets/images/cards/tygr-card.png`,
+    petIcon: ''
+  },
+  {
+    id: 9,
+    petImg: `${BASE}assets/images/cards/red-panda.jpg`,
+    petIcon: ''
+  },
+  {
+    id: 10,
+    petImg: `${BASE}assets/images/cards/gorilla.jpg`,
+    petIcon: ''
+  },
+  {
+    id: 11,
+    petImg: `${BASE}assets/images/cards/elephant-card.jpg`,
+    petIcon: ''
+  },
+  {
+    id: 12,
+    petImg: `${BASE}assets/images/cards/otter-card.jpg`,
+    petIcon: ''
+  },
+  {
+    id: 13,
+    petImg: `${BASE}assets/images/cards/bengal-tiger.jpg`,
+    petIcon: ''
+  },
+  {
+    id: 14,
+    petImg: `${BASE}assets/images/cards/wolf-card.jpg`,
+    petIcon: ''
+  },
+  {
+    id: 15,
+    petImg: `${BASE}assets/images/cards/fox-card.jpg`,
+    petIcon: ''
+  },
+  {
+    id: 16,
+    petImg: `${BASE}assets/images/cards/grizzly-card.jpg`,
+    petIcon: ''
+  },
+  {
+    id: 17,
+    petImg: `${BASE}assets/images/cards/dolphin-card.jpg`,
+    petIcon: ''
+  },
+  {
+    id: 18,
+    petImg: `${BASE}assets/images/cards/leopard-card.jpg`,
+    petIcon: ''
+  },
+  {
+    id: 19,
+    petImg: `${BASE}assets/images/cards/polar-bear.jpg`,
+    petIcon: ''
+  },
+  {
+    id: 20,
+    petImg: `${BASE}assets/images/cards/jaguar.jpg`,
+    petIcon: ''
+  },
+   {
+    id: 21,
+    petImg: `${BASE}assets/images/cards/ring-tailed-lemur.jpg`,
+    petIcon: ''
+  },
+  {
+    id: 22,
+    petImg: `${BASE}assets/images/cards/white-rhinoceros.jpg`,
+    petIcon: ''
+  },
+  {
+    id: 23,
+    petImg: `${BASE}assets/images/cards/arctic-fox.jpg`,
+    petIcon: ''
+  },
+  {
+    id: 24,
+    petImg: `${BASE}assets/images/cards/saltwater-crocodile.jpg`,
+    petIcon: ''
+  },
+  {
+    id: 25,
+    petImg: `${BASE}assets/images/cards/scarlet-macaw.jpg`,
+    petIcon: ''
+  },
+  {
+    id: 26,
+    petImg: `${BASE}assets/images/cards/komodo-dragon.jpg`,
+    petIcon: ''
+  },
+  {
+    id: 27,
+    petImg: `${BASE}assets/images/cards/sloth.jpg`,
+    petIcon: ''
+  },
+  {
+    id: 28,
+    petImg: `${BASE}assets/images/cards/cheetah.jpg`,
+    petIcon: ''
+  }
+]
