2822513027eae6184c2f7e099e139e035f97cef0
vladaworkflow-ops <vlada.work.flow@gmail.com>
2026-03-24 18:04:11 +0100

    feat: add event handler for button donate, implement input text handler

--- src/scripts/popup/donation-popup.ts
+++ src/scripts/popup/donation-popup.ts
@@ -1,30 +1,57 @@
-const togetherBtn = document.querySelectorAll('.cam-button') as  NodeListOf<HTMLDivElement>;
-const footerDonateBtn = document.querySelectorAll('.footer-button-donate') as NodeListOf<HTMLButtonElement>;
+const togetherOpenBtn = document.querySelectorAll('.cam-button') as  NodeListOf<HTMLDivElement>;
+const togetherBtn = document.querySelectorAll('.together-donate-btn') as NodeListOf<HTMLButtonElement>;
 const overlay = document.querySelector('.overlay') as  HTMLElement;
 const modal = document.querySelector('.modal-together-container') as  HTMLElement;
 const closeModalBtn = document.querySelector('.close-modal-together') as  HTMLElement;
-const donationBtn = document.querySelectorAll('.donation-btn') as  NodeListOf<HTMLDivElement>;
+const donationBtn = document.querySelectorAll('.donation-btn') as NodeListOf<HTMLButtonElement>;
 const modalDonation = document.querySelector('.modal-donation-container') as  HTMLElement;
+const donationBtnIcon = document.querySelector('#donation-btn-icon') as HTMLElement;
+const donationBtnInput = document.querySelector('#donation-btn-input') as HTMLInputElement;
 
-footerDonateBtn.forEach((btn: HTMLButtonElement) => {
-  if(!overlay && !modalDonation) return;
+const initialValue = donationBtnInput.value;
 
-  btn.addEventListener('click', () => {
-    overlay.classList.add('active-modal');
-    modalDonation.classList.add('active-modal');
-  })
+donationBtnInput.addEventListener("blur", () => {
+  if (donationBtnInput.value.trim() === "") {
+    donationBtnInput.value = initialValue;
+  }
+});
+
+donationBtnInput.addEventListener("focus", () => {
+  donationBtnInput.value = "";
 })
+
+function handlerDonationModal(){
+  overlay.classList.add('active-modal');
+  modalDonation.classList.add('active-modal');
+}
 
 donationBtn.forEach((btn: HTMLElement) => {
   if(!overlay && !modalDonation) return
 
   btn.addEventListener('click', () => {
-    overlay.classList.add('active-modal');
-    modalDonation.classList.add('active-modal');
+    handlerDonationModal()
   })
 })
 
 togetherBtn.forEach((btn: HTMLElement) => {
+  if(!overlay && !modalDonation) return
+
+  btn.addEventListener('click', () => {
+    handlerDonationModal();
+    modal.classList.remove('active-modal');
+  })
+})
+
+
+if (donationBtnIcon && overlay && modalDonation) {
+  donationBtnIcon.addEventListener('click', (event) => {
+    event.stopPropagation();
+
+    handlerDonationModal()
+  });
+}
+
+togetherOpenBtn.forEach((btn: HTMLElement) => {
   btn.addEventListener('click', () => {
     overlay.classList.add('active-modal');
     modal.classList.add('active-modal');
