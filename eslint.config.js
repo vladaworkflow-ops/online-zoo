b0fbfd6542803655347647c6e39c9e31a97b7d4b
vladaworkflow-ops <vlada.work.flow@gmail.com>
2026-04-03 00:05:13 +0200

    chore: install prettier and add configurations

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,24 @@
-import tseslint from "@typescript-eslint/eslint-plugin";
-import parser from "@typescript-eslint/parser";
+import tseslint from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
+import prettier from 'eslint-config-prettier';
 
 export default [
   {
-    files: ["src/**/*.ts"],
+    files: ['src/**/*.ts', 'src/**/*.js'],
     languageOptions: {
       parser,
       parserOptions: {
-        project: "./tsconfig.json"
-      }
+        project: './tsconfig.json',
+      },
     },
     plugins: {
-      "@typescript-eslint": tseslint
+      '@typescript-eslint': tseslint,
     },
     rules: {
       ...tseslint.configs.recommended.rules,
 
-      "@typescript-eslint/no-explicit-any": "error"
-    }
-  }
+      '@typescript-eslint/no-explicit-any': 'error',
+    },
+  },
+  prettier,
 ];
